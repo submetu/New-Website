73e82f5d84cbef53b8f8546e0246a473aa484b0a
M Subhan Naeem <sub.metu@gmail.com>
2018-04-19 09:14:25 +0300

    using exec

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -9,24 +9,20 @@ router.post('/github', function(req, res, next) {
     var branch = req.body.ref;
 
     if(branch.indexOf('master') > -1 && commits.length > 0 && sender.login === 'submetu'){
-        deploy();
-        res.send('200');
-        
-        
+        deploy(res);
     }
-    else{
-        res.send(500);
-    }
+   
 
 });
 
-function deploy(){
-    var child = childProcess.spawn('./deploy.sh', {
-        stdio: 'inherit',
-        shell: true,
-        cwd: '/home'
+function deploy(res){
+    childProcess.exec('cd /home && ./deploy.sh', function(err, stdout, stderr){
+        if (err) {
+         console.error(err);
+         return res.send(500);
+        }
+        res.send(200);
       });
-      child.unref();
 }
 
 module.exports = router;
