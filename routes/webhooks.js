254a7e88afa065eb456f796ae6ba27766ebd02c6
M Subhan Naeem <sub.metu@gmail.com>
2018-04-19 08:35:53 +0300

    let fix

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/github', function(req, res, next) {
-    let commits = req.body.commits; 
-    let sender = req.body.sender;
+    var commits = req.body.commits; 
+    var sender = req.body.sender;
 
     if(commits.length > 0 && sender.login === 'submetu'){
         console.log('RESTART SERVER!!!');
