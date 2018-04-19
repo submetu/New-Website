2dd432d438ffe0c739e0d3ec360fd0686a984692
M Subhan Naeem <sub.metu@gmail.com>
2018-04-19 09:03:46 +0300

    test 16

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -11,6 +11,7 @@ router.post('/github', function(req, res, next) {
     if(branch === 'refs/heads/master' && commits.length > 0 && sender.login === 'submetu'){
         deploy();
         res.send('200');
+        
     }
 
 
